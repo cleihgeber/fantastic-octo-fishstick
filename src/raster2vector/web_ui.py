b080086dfb6873817eb75d79d111a743df3ce667
cleihgeber <109441444+cleihgeber@users.noreply.github.com>
2026-01-28 16:11:33 -0500

    Update web_ui.py

--- src/raster2vector/web_ui.py
+++ src/raster2vector/web_ui.py
@@ -429,8 +429,6 @@ def create_web_ui() -> "gr.Blocks":
                     gr.Image(
                         value=logo_path,
                         show_label=False,
-                        show_share_button=False,
-                        container=False,
                         height=150,
                     )
             with gr.Column(scale=3):
